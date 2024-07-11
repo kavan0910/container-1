e66d3d8798dcd338c500617b215b596c15faa2b1
kavankumar <150102479+KavanBrahmbhatt0910@users.noreply.github.com>
2024-07-11 00:35:50 -0300

    code added

---
+++ src/main/java/com/kavankumar/ms1/request/CalculateRequest.java
@@ -0,0 +1,22 @@
+package com.kavankumar.ms1.request;
+
+public class CalculateRequest {
+    private String file;
+    private String product;
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+}
